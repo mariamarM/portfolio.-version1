8da69a35ec75c48cb280f5f0770778095b27502c
mihermanogordo <andreo.olivella@gmail.com>
2026-04-15 13:49:37 +0200

    nvbar static in every page gooogirl

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,13 @@ import styles from '../styles/navbar.module.css'
 
 
 export default function Navbar() {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
   return (
-    <nav className={`${styles.navbar} ${isHome ? styles.homeNavbar : ''}`}>
+    <nav className={styles.navbar}>
       <Link to="/">MARIA ANDREO</Link>
       <div className={styles.navLinks}>
-      <Link to="/projects">PROJECTS</Link>
-      <Link to="/contact">CONTACT</Link>
-      <Link to="/about">ABOUT ME</Link>
+        <Link to="/projects">PROJECTS</Link>
+        <Link to="/about">ABOUT ME</Link>
+        <Link to="/contact">CONTACT</Link>
       </div>
     </nav>
   )
