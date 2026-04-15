57c4271a2768a0a6ecb1c7d64ed6070eea8dcc35
mihermanogordo <andreo.olivella@gmail.com>
2026-04-15 13:20:59 +0200

    navbar adjust

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styles from '../styles/navbar.module.css'
 
 
 export default function Navbar() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${isHome ? styles.homeNavbar : ''}`}>
       <Link to="/">MARIA ANDREO</Link>
       <div className={styles.navLinks}>
       <Link to="/projects">PROJECTS</Link>
